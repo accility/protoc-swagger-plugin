259270547b99811c24d9f1d917d164d7f0f47209
tscpp <elias.skogevall@gmail.com>
2020-10-04 12:09:24 +0200

    Update protoc-gen-swagger.ts

--- src/lib/protoc-gen-swagger.ts
+++ src/lib/protoc-gen-swagger.ts
@@ -1,15 +1,16 @@
 import { ProtocOptions, protoc } from '@accility/protoc-tools';
 import * as apis from 'google-proto-files';
 import * as path from 'path';
+import * as os from 'os';
 
-const extension = process.platform === 'win32' ? '.exe' : '';
-export function fromProto(options: ProtocOptions) : Promise<void> {
-  options.includeDirs.push(path.resolve(apis.getProtoPath(), '..'));
-  options.plugin = plugin;
-  return protoc(options);
+const extension = os.platform().toLowerCase() === 'win32' ? '.exe' : '';
+export function fromProto(options: ProtocOptions): Promise<void> {
+	options.includeDirs.push(path.resolve(apis.getProtoPath(), '..'));
+	options.plugin = plugin;
+	return protoc(options);
 }
 
 export const plugin = {
-    name: 'swagger',
-    path: path.resolve(__dirname, '../../native/bin', process.platform, process.arch, 'protoc-gen-swagger' + extension),
+	name: 'swagger',
+	path: path.resolve(__dirname, '../../native/bin', os.platform().toLowerCase(), os.arch().toLowerCase(), 'protoc-gen-swagger' + extension),
 }
