e519b947020143188e4c8f0b18adf573d7d6f3cd
Elias Skogevall <elias.skogevall@gmail.com>
2020-10-15 15:36:31 +0200

    update binaries to 2.0.0

--- src/lib/protoc-gen-swagger.ts
+++ src/lib/protoc-gen-swagger.ts
@@ -3,13 +3,14 @@ import * as apis from 'google-proto-files';
 import * as path from 'path';
 
 const extension = process.platform === 'win32' ? '.exe' : '';
-export function fromProto(options: ProtocOptions) : Promise<void> {
-  options.includeDirs.push(path.resolve(apis.getProtoPath(), '..'));
-  options.plugin = plugin;
-  return protoc(options);
+
+export function fromProto(options: ProtocOptions): Promise<void> {
+	options.includeDirs.push(path.resolve(apis.getProtoPath(), '..'));
+	options.plugin = plugin;
+	return protoc(options);
 }
 
 export const plugin = {
-    name: 'swagger',
-    path: path.resolve(__dirname, '../../native/bin', process.platform, process.arch, 'protoc-gen-swagger' + extension),
+	name: 'swagger',
+	path: path.resolve(__dirname, '../../native/bin', process.platform, 'protoc-gen-swagger' + extension),
 }
